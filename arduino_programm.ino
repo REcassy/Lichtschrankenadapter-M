7ad3019011e07ab6e01dd89ec42473a815515113
ndon-phys <niklas@donocik.name>
2024-08-11 22:16:24 +0200

    Add files via upload

---
+++ arduino_programm.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+uint8_t answer[4]={0x00,0x08, 0x00, 0x8F}; // Dieser Code ist die Katalognummer in Hexadezimalformat
+
+void I2C_RxHandler(int numBytes)
+{
+  Wire.write(answer, sizeof(answer)); // Auf jede Frage mit unserer Nummer reagieren
+}
+ 
+void setup() {
+  pinMode(7, OUTPUT);
+  Wire.setClock(250000); // 250 khZ Frequnz
+  Wire.begin(0x50); // Auf Adresse 50h reagieren
+  Wire.onRequest(I2C_RxHandler);
+}
+ 
+void loop() {
+  // Nothing To Be Done Here
+  digitalWrite(7, HIGH);
+  delay(750);
+  digitalWrite(7, LOW);
+  delay(750);
+}
