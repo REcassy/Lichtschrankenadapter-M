eeb49aa331cc73fc19e8363b34daa7b8f671c9ac
ndon-phys <niklas@donocik.name>
2024-09-08 09:49:43 +0200

    Create ATtiny.ino

---
+++ ATtiny/ATtiny.ino
@@ -0,0 +1,15 @@
+#include <Wire.h> // I2C Library
+uint8_t answer[4]={0x00,0x08, 0x00, 0x8F}; // This code is the sensor catalogue number
+
+void I2C_RxHandler(int numBytes) {
+  Wire.write(answer, sizeof(answer)); // Simply answer every request with out number
+}
+ 
+void setup() {
+  Wire.setClock(250000); // 250 khZ frequency
+  Wire.begin(0x50); // adress 0x50
+  Wire.onRequest(I2C_RxHandler);
+}
+ 
+void loop() { // nothing to do
+}
