28bb7fe20b241587d8b4ed2603cf31f14c8065af
ndon-phys <niklas@donocik.name>
2024-09-08 09:49:15 +0200

    Update Arduino-Simulator.ino

--- Arduino-Simulator.ino
+++ Arduino-Simulator.ino
@@ -1,20 +1,18 @@
-#include <Wire.h>
-uint8_t answer[4]={0x00,0x08, 0x00, 0x8F}; // Dieser Code ist die Katalognummer in Hexadezimalformat
+#include <Wire.h> // I2C Library
+uint8_t answer[4]={0x00,0x08, 0x00, 0x8F}; // This code is the sensor catalogue number
 
-void I2C_RxHandler(int numBytes)
-{
-  Wire.write(answer, sizeof(answer)); // Auf jede Frage mit unserer Nummer reagieren
+void I2C_RxHandler(int numBytes) {
+  Wire.write(answer, sizeof(answer)); // Simply answer every request with out number
 }
  
 void setup() {
-  pinMode(7, OUTPUT);
-  Wire.setClock(250000); // 250 khZ Frequnz
-  Wire.begin(0x50); // Auf Adresse 50h reagieren
+  Wire.setClock(250000); // 250 khZ frequency
+  Wire.begin(0x50); // adress 0x50
   Wire.onRequest(I2C_RxHandler);
 }
- 
+
 void loop() {
-  // Optional zur Simulierung einer Lichtschranke
+  // Toggling pin 7
   digitalWrite(7, HIGH);
   delay(750);
   digitalWrite(7, LOW);
